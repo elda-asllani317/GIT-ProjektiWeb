d9ff0f75e085b51f60e8cf0651c036b0075f1acd
MerisaShallci <shallcimerisa@gmail.com>
2024-11-29 13:53:34 +0100

    commit

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -1,4 +1,3 @@
-
 function nextStep() {
     clearErrors();
     let firstName = document.getElementById('firstName').value;
@@ -9,8 +8,7 @@ function nextStep() {
       if (!lastName) document.getElementById('errorLastName').textContent = "Please write your surname.";
       return;
     }
-  
-    // Hide the first step and show the second step
+
     document.getElementById('step1').style.display = 'none';
     document.getElementById('step2').style.display = 'block';
   }
