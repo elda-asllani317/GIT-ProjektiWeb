49ec064c6b78b22efc46c860dd79d544c6e79936
elda-asllani317 <eldaasllani7@gmail.com>
2025-01-25 17:42:32 +0100

    commit

--- php/index.php
+++ php/index.php
@@ -4,3 +4,30 @@ include 'db.php';
 $sql = "SELECT * FROM pricing_plans";
 $result = $conn->query($sql);
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pricing Plans</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            background-color: #f9f9f9;
+        }
+
+        h1 {
+            color: #333;
+            text-align: center;
+        }
+
+        a {
+            text-decoration: none;
+            color: #007BFF;
+            font-weight: bold;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
